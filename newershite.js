bf3186a292a35247c7697a7b1fd95a4861c71a91
maniganiga <enkhmunkh.e1@orkhonschool.edu.mn>
2024-08-14 11:00:06 +0800

    holyygawddamnn

---
+++ newershite.js
@@ -0,0 +1,19 @@
+// const name = 'Enkhmunkh'
+// const age ='16'
+
+
+
+
+// console.log('Sain baina uu namaig ' + name + ' bi ' +age+ ' nastai.');
+
+// console.log('hello','string')
+// console.log(true,  )
+// console.log("Name",  )
+// console.log(33,  )
+// console.log("90",  )
+// console.log(90+"77",  )
+// console.log( {name:'Anu', age:15},  )
+// console.log(false,  )
+// console.log(1655/5,  )
+// console.log(["javascript", "html", "css"])
+
