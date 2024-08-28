6be2149f2bee2c05b3f14061767ef763c823e26e
maniganiga <enkhmunkh.e1@orkhonschool.edu.mn>
2024-08-28 20:21:54 +0800

    jss

--- newershite.js
+++ newershite.js
@@ -1,63 +1,144 @@
-const name = 'Enkhmunkh'
-const aje ='16'
+// const name = 'Enkhmunkh'
+// const aje ='16'
 
- console.log('Sain baina uu namaig ' + name + ' gedeg . Bi ' +aje+ ' nastai.')
+//  console.log('Sain baina uu namaig ' + name + ' gedeg . Bi ' +aje+ ' nastai.')
 
-console.log('hello','string')
-console.log(true, 'boolean'  )
-console.log("Name", 'variable'  )
-console.log(33, 'number' )
-console.log("90", 'string'  )
-console.log(90+"77", 'number and string' )
-console.log( {name:'Anu', age:15}, 'object' )
-console.log(false, 'boolean' )
-console.log(1655/5, 'number' )
-console.log(["javascript", "html", "css"] ,'array')
-let x= 'water'
-let y= 'cola'
-let z= 'sprite'
-let w;
-w=x;
-x=y;
-y=z;
-z=w;
-console.log(x,y,z);
-let b=6;
-let c=3;
-let d=2;
-let a=b*c-d;
-console.log(a);
-let day=2*24;
-let time=10;
-let totaltime=day+time;
-console.log(totaltime);
-let bornyear=2008;
-let futureyear=2099;
-let age =futureyear-bornyear;
-console.log(age);
-let birthdate=2008 ;
+// console.log('hello','string')
+// console.log(true, 'boolean'  )
+// console.log("Name", 'variable'  )
+// console.log(33, 'number' )
+// console.log("90", 'string'  )
+// console.log(90+"77", 'number and string' )
+// console.log( {name:'Anu', age:15}, 'object' )
+// console.log(false, 'boolean' )
+// console.log(1655/5, 'number' )
+// console.log(["javascript", "html", "css"] ,'array')
+// let x= 'water'
+// let y= 'cola'
+// let z= 'sprite'
+// let w;
+// w=x;
+// x=y;
+// y=z;
+// z=w;
+// console.log(x,y,z);
+// let b=6;
+// let c=3;
+// let d=2;
+// let a=b*c-d;
+// console.log(a);
+// let day=2*24;
+// let time=10;
+// let totaltime=day+time;
+// console.log(totaltime);
+// let bornyear=2008;
+// let futureyear=2099;
+// let age =futureyear-bornyear;
+// console.log(age);
+// let birthdate=2008 ;
 
-let bornmonth=1*31;
-let bornday=20;
+// let bornmonth=1*31;
+// let bornday=20;
 
-let today=2024;
-let days=(today-birthdate)*365+bornmonth+bornday
-console.log(days);
+// let today=2024;
+// let days=(today-birthdate)*365+bornmonth+bornday
+// console.log(days);
 
-let too = 5678
-// 4
-let g=too%10;
-console.log('g='+g);
+// let too = 5678
+// // 4
+// let g=too%10;
+// console.log('g='+g);
 
-// 30 
-let h=too%100-g;
-console.log('h='+h);
+// // 30 
+// let h=too%100-g;
+// console.log('h='+h);
 
-let j=(too%1000-h-g);
-console.log(j);
+// let j=(too%1000-h-g);
+// console.log(j);
 
-let k=too%10000-j-h-g;
-console.log(k);
+// let k=too%10000-j-h-g;
+// console.log(k);
 
-let answer= g*1000 +h*10+j/10+k/1000;
-console.log(answer);
+// let answer= g*1000 +h*10+j/10+k/1000;
+// console.log(answer);
+
+// 2ND HICHEEL
+
+let a =1
+let b = 78
+if (a>b) {
+    console.log('a is more');
+    
+} else {
+    console.log('b is more');
+}
+
+let x = 5781
+if (x%2===0) {
+    console.log('is even');
+    
+} else {
+console.log('is odd');
+}
+
+let newpassword= '69'
+let confirmnewpassword= '69'
+if (newpassword===confirmnewpassword) {
+    console.log('password passed');
+    
+} else {
+    console.log('denied pass');
+}
+let grade = '0'
+if (grade>=90) {
+    console.log("a");
+}else if (grade<=89 &&grade >=80){
+    console.log('b');
+    
+}
+else if (grade<=79 &&grade>=70){
+    console.log('c');
+    
+}
+else if (grade<=69 &&grade>=60){
+    console.log('d');
+    
+}
+else {
+    console.log('f');
+    
+}
+
+let piecofshitnumber = "shd"
+if (piecofshitnumber%3===0 && piecofshitnumber%5===0) {
+    console.log('shitzngiggles');
+    
+} 
+      else if (piecofshitnumber%3===0 && piecofshitnumber%5>=1) {
+      console.log('shitz');
+    
+}
+else if (piecofshitnumber%3>=1 && piecofshitnumber%5===0) {
+    console.log('giggles');
+     
+ }
+else if (piecofshitnumber%3>=1 && piecofshitnumber%5>=1) {
+    console.log('buuz');
+     
+ }
+ else {
+    console.log('gigglesnshitz');
+    
+ }
+ let fastnfurious = 1
+ let fastnfuriousspeed= 80
+ let speedanswer = (fastnfurious-fastnfuriousspeed)/5
+ console.log(speedanswer);
+ 
+ if (speedanswer >=5) {
+    console.log('your license is revoked');
+      
+ } else {
+    console.log('you good fam ');
+    
+ }
